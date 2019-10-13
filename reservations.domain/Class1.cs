02b5809c4bf257a3951a11352d3f76cf1fc4f016
Chris Graham <chrissygraham72@gmail.com>
2019-10-13 16:00:14 +0100

    Initial commit

---
+++ reservations.domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace reservations.domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ reservations.infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace reservations.infrastructure
+{
+    public class Class1
+    {
+    }
+}
